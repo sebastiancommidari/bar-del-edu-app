8a182025b2f30afd1e8d72197cbebd1c386b0a8e
Sebastián Ariel Commidari <100450059+sebastiancommidari@users.noreply.github.com>
2024-12-30 00:52:59 -0300

    Modifiqué botón agregar al carrito

--- src/Home.jsx
+++ src/Home.jsx
@@ -135,7 +135,7 @@ function Home({ addToCart }) {
                       <IconButton icon={<AddIcon />} onClick={() => handleQuantityChange(product.id, 1)} />
                     </HStack>
                   </Center>
-                  <Button mt={2} colorScheme="teal" onClick={() => addToCart({ ...product, quantity: quantity[product.id] || 1 })}>Agregar al Carrito</Button>
+                  <Button mt={2} fontSize="14px" colorScheme="teal" onClick={() => addToCart({ ...product, quantity: quantity[product.id] || 1 })}>Sumar al Carrito</Button>
                 </VStack>
               </Flex>
             </Box>
