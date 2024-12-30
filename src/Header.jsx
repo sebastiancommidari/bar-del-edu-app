b986ee4b720c0ed475e0bf77b8cdffefbe9a9d1b
Sebastián Ariel Commidari <sebastian.commidari@gmail.com>
2024-12-30 18:45:12 -0300

    Mejoras header

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <Box as="header" width="100%" bg="teal.500" color="white" p={4}>
       <Flex justifyContent="center">
         <img src="https://svgsilh.com/svg/2023997.svg" width={55} alt="" />
-        <Text fontSize="34px" fontWeight="bold" marginLeft={5} marginRight={5}>
+        <Text fontSize="32px" fontWeight="bold" marginLeft={5} marginRight={5}>
           El Bar del Edu
         </Text>
         <img src="https://svgsilh.com/svg/2023997.svg" width={55} alt="" />
